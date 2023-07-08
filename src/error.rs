d574f2713f3e18d8478c041b256ce2c7e1c1c338
Sayan Mallick <snyxmk@gmail.com>
2023-07-08 12:04:33 +0530

    client & server errors

--- src/error.rs
+++ src/error.rs
@@ -5,7 +5,7 @@ use axum::{
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, strum_macros::AsRefStr)]
 pub enum Error {
     LoginFail,
 
@@ -30,6 +30,44 @@ impl IntoResponse for Error {
     fn into_response(self) -> Response {
         println!("->> {:12} - {self:?}", "ERROR");
 
-        (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+        // Create a placeholder response
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+        // Insert the Error into the response
+        response.extensions_mut().insert(self);
+
+        response
+    }
+}
+
+#[derive(Debug, strum_macros::AsRefStr)]
+#[allow(non_camel_case_types)]
+pub enum ClientError {
+    LOGIN_FAIL,
+    NO_AUTH,
+    INVAID_PARAMS,
+    SERVER_ERROR,
+}
+
+impl Error {
+    pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+        #[allow(unreachable_patterns)]
+        match self {
+            // Login
+            Self::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
+
+            // Auth
+            Self::AuthFailCtxNotInRequestExt
+            | Self::AuthFailNoAuthTokenCookie
+            | Self::AuthFailTokenWrongFormat => (StatusCode::FORBIDDEN, ClientError::NO_AUTH),
+
+            // Model
+            Self::TicketDeleteFailIdNotFound { .. } => {
+                (StatusCode::BAD_REQUEST, ClientError::INVAID_PARAMS)
+            }
+
+            // Fallback
+            _ => (StatusCode::INTERNAL_SERVER_ERROR, ClientError::SERVER_ERROR),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@ use axum::{
     middleware,
     response::{Html, IntoResponse, Response},
     routing::{get, get_service},
-    Router,
+    Json, Router,
 };
 use serde::Deserialize;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
+use uuid::Uuid;
 
 mod ctx;
 mod error;
@@ -51,10 +52,40 @@ fn router_static() -> Router {
 }
 
 async fn main_response_mapper(res: Response) -> Response {
-    // println!("->> {:12} - main_response_mapper - {res:?}", "RES_MAPPER");
+    println!("->> {:12} - main_response_mapper", "RES_MAPPER");
+    let uuid = Uuid::new_v4();
+
+    // Get the eventual response error
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    // If client error, build the new response
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = serde_json::json!({
+                "error": {
+                    "type": client_error.as_ref(),
+                    "req_uuid": uuid.to_string(),
+                }
+            });
+
+            println!(
+                "->> {:12} - client_error_body - {client_error_body:?}",
+                "CLIENT_ERROR",
+            );
+
+            // Build the new response
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    println!(
+        "->> {:12} - error_response - {uuid:?} - {service_error:?}",
+        "SERVICE_ERROR",
+    );
 
     println!();
-    res
+    error_response.unwrap_or(res)
 }
 
 #[tokio::main]
